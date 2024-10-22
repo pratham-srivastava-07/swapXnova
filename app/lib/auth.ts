0ecd41c048137833e41d4bf1b68f5e9bb32140c2
Krish <prathamrajsri2003@gmail.com>
2024-10-22 15:14:55 +0530

    dep change

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prismaClient } from "./db";
 import bcrypt from "bcrypt"
